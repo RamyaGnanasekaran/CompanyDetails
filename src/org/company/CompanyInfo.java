ef09f6bf3a7ef241a72ef110faba47a00da02606
ramyagnanasekaran <ramyasekar02@gmail.com>
2019-01-29 22:33:11 +0530

    ur commit message

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+		public void companyName() {
+			
+			System.out.println("Hexa");
+		}
+		public void companyId() {
+			
+			System.out.println("76545");
+		}
+		public void companyAddress() {
+			
+			System.out.println("Siruseri");
+		}
+		public static void main(String[] args) {
+		CompanyInfo comp = new CompanyInfo();
+		comp.companyName();
+		comp.companyId();
+		comp.companyAddress();
+	
+		}
+	}
